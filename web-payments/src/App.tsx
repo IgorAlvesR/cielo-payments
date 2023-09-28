daacab033c950ea4616d3639e0eca3312e495bf4
Igor <igor.tub18@gmail.com>
2023-09-28 18:46:45 -0300

    refactor: melhora estrutura e organização do projeto.

--- web-payments/src/App.tsx
+++ web-payments/src/App.tsx
@@ -1,17 +1,13 @@
-import { BrowserRouter } from 'react-router-dom'
 import { Navbar } from './components/Navbar'
-import { ThemeProvider } from './components/theme-provider'
 import Router from './pages/router'
 
 export function App() {
   return (
-    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <BrowserRouter>
-        <Navbar />
-        <main className="mt-[286px] sm:mt-24 max-w-7xl my-0 px-16 mx-auto">
-          <Router />
-        </main>
-      </BrowserRouter>
-    </ThemeProvider>
+    <>
+      <Navbar />
+      <main className="mt-[286px] pb-24 sm:mt-24 max-w-7xl my-0 px-16 mx-auto">
+        <Router />
+      </main>
+    </>
   )
 }
