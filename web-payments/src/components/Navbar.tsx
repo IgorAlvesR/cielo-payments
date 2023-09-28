1dcc9fa5591f09bc7dfadbd7213d0e3daae42258
Igor <igor.tub18@gmail.com>
2023-09-28 18:47:45 -0300

    feat: exibe rota ativa com uma cor diferente.

--- web-payments/src/components/Navbar.tsx
+++ web-payments/src/components/Navbar.tsx
@@ -1,8 +1,11 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { Logo } from './Logo'
 import { ModeToggle } from './mode-toggle'
 
 export function Navbar() {
+  const location = useLocation()
+  const activeRoute = location.pathname
+
   return (
     <header className="fixed inset-x-0 top-0 w-full z-10 flex sm:h-16 px-12 bg-blue-200 justify-center dark:bg-blue-300 dark:text-zinc-800">
       <div className="max-w-6xl flex flex-col items-center justify-between sm:flex-row flex-1">
@@ -16,10 +19,24 @@ export function Navbar() {
           >
             <ul className="flex items-center gap-4 flex-col uppercase font-semibold sm:flex-row sm:gap-8">
               <li className="hover:text-zinc-800/70 duration-100">
-                <Link to="/">Transações</Link>
+                <Link
+                  to="/"
+                  className={`${
+                    activeRoute === '/' ? 'text-zinc-800/70' : ''
+                  } `}
+                >
+                  Transações
+                </Link>
               </li>
               <li className="hover:text-zinc-800/70 duration-100">
-                <Link to="/metrics">Métricas</Link>
+                <Link
+                  to="/metrics"
+                  className={`${
+                    activeRoute === '/metrics' ? 'text-zinc-800/70' : ''
+                  } `}
+                >
+                  Métricas
+                </Link>
               </li>
             </ul>
           </nav>
