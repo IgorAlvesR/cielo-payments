0e9a526bc2c075fe9e4451871f0e066808ac67d2
Igor <igor.tub18@gmail.com>
2023-09-28 22:36:33 -0300

    feat: melhora estilização do botão de troca de tema.

--- web-payments/src/components/mode-toggle.tsx
+++ web-payments/src/components/mode-toggle.tsx
@@ -12,13 +12,12 @@ export function ModeToggle() {
       data-testid="button-theme"
       className="border border-zinc-800 bg-zinc-100 dark:bg-zinc-100 hover:bg-zinc-300 dark:hover:bg-zinc-300 dark:text-zinc-800"
       variant="outline"
-      size="icon"
       onClick={() => setTheme(isDark ? 'light' : 'dark')}
     >
       {isDark ? (
-        <MdDarkMode className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 " />
+        <MdDarkMode className="h-4 w-4" />
       ) : (
-        <BsSun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <BsSun className="h-4 w-4" />
       )}
       <span className="sr-only">Toggle theme</span>
     </Button>
